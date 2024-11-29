435af1cdced0c22792cbe4cf9f20fad6f70df473
jimbucktwo <jimmytraann@gmail.com>
2024-11-28 17:39:00 -0800

    added queries

--- TCPServer.py
+++ TCPServer.py
@@ -1,6 +1,7 @@
 import socket
 import random
 
+
 TCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 hostname = socket.gethostname()
 IPaddress = socket.gethostbyname(hostname)
@@ -14,7 +15,18 @@ incomingSocket, incomingAdress = TCPSocket.accept()
 
 while True:
     myData = incomingSocket.recv(1024).decode()
-    if myData:
+
+    #Change the following code to process the data received from the client
+    if myData == '1':
+        #Process average moisture inside the kitchen fridge in the past three hours
+        print(f'Received "{myData}" from the client')
+        incomingSocket.sendall(myData.upper().encode())
+    elif myData == '2':
+        #Process water consumption per cycle in my smart dishwasher
+        print(f'Received "{myData}" from the client')
+        incomingSocket.sendall(myData.upper().encode())
+    elif myData == '3':
+        #process electricity consumption among the IoT devices
         print(f'Received "{myData}" from the client')
         incomingSocket.sendall(myData.upper().encode())
     else:
